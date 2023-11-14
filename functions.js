1dc891251c92580eab7391aefdb31cb7c8b102fe
Chris <104102589+cjmonahan10@users.noreply.github.com>
2023-11-13 20:31:52 -0500

    Completed functionality of tictactoe game Game accepts input through UI and stops when a player wins. Reset button resets board and makes player1 the active player

--- functions.js
+++ functions.js
@@ -1,12 +1,13 @@
 
 // Player objects
-// const players = (function(){
+const players = (function(){
 
-//     let player1 = {name: "Player 1", value: 1};
-//     let player2 = {name: "PLayer 2", value: 2};
+    let players = [{name: "Player 1", value: 1}, {name: "Player 2", value: 2}];
 
-//     const getName = ()
-// })();
+    const getPlayer = (index) => players[index];
+
+    return {getPlayer};
+})();
 
 
 // Gameboard object
@@ -34,28 +35,24 @@ const gameboard = (function () {
 // Game Controller
 const gameController = (function () {
     
-    let haveWinner = false;
+    let haveWinner = "";
 
-    let players = [{name: "Player 1", value: 1}, {name: "Player 2", value: 2}];
+    const player1 = players.getPlayer(0);
+    const player2 = players.getPlayer(1);
 
-    let activePlayer = players[0];
+    let activePlayer = player1;
 
     const changePlayer = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+        activePlayer = activePlayer === player1 ? player2 : player1;
     }
 
     const playMove = (index) => {
         if (gameboard.getCellValue(index) === ""){
             gameboard.changeCell(index, activePlayer.value);
             checkWin();
-            console.log(haveWinner);
             changePlayer();
         }
     }
-
-    const arraySearch = (indices, condition) => {
-        return condition.every(index => indices.includes(index));
-    };
 
     const checkWin = () => {
         const indices = [];
@@ -65,7 +62,6 @@ const gameController = (function () {
             indices.push(idx);
             idx = board.indexOf(activePlayer.value, idx + 1);
         }
-        console.log(indices);
 
         const winConditions = [
             [0, 3, 6],
@@ -78,23 +74,91 @@ const gameController = (function () {
             [2, 4, 6]
         ];
 
+        const arraySearch = (indices, condition) => {
+            return condition.every(index => indices.includes(index));
+        };
+
         winConditions.forEach((condition) => {
             if (arraySearch(indices, condition) === true){
-                haveWinner = true;
+                haveWinner = activePlayer.name;
             }
         });
-
     }
 
-    return {
-        playMove
-    };
+    const reset = () => {
+        activePlayer = player1;
+        haveWinner = "";
+    }
+
+    const getWinner = () => haveWinner;
+
+    const getActivePlayer = () => activePlayer;
+
+    return {playMove, reset, getWinner, getActivePlayer};
 })();
 
 
 // Screen Controller
 const screenController = (function () {
+    
+    const playerTurn = document.querySelector(".turn");
+    const cellContainer = document.querySelector(".container");
 
+    const updateScreen = () => {
+
+        cellContainer.textContent = "";
+
+        playerTurn.textContent = `It's ${gameController.getActivePlayer().name}'s turn!`;
+
+        const board = gameboard.getBoard();
+
+        board.forEach((cell, index) => {
+
+            const cellButton = document.createElement("button");
+            cellButton.classList.add("cell");
+            cellButton.dataset.index = index;
+            
+            if (cell === 1){
+                cellButton.textContent = "X";
+            } else if (cell === 2){
+                cellButton.textContent = "O";
+            }
+
+            cellContainer.appendChild(cellButton);
+        })
+        endGame();
+    };
+
+    const endGame = () => {
+        if(gameController.getWinner() !== ""){
+            cellContainer.removeEventListener("click", clickHandler);
+            playerTurn.textContent = `${gameController.getWinner()} wins!`;
+        }
+    }
+
+    const clickHandler = (e) => {
+        const selectedCell = e.target.dataset.index;
+
+        console.log(selectedCell);
+        if(!selectedCell) return;
+
+        gameController.playMove(selectedCell);
+        updateScreen();
+    }
+
+    const resetScreen = () => {
+        gameController.reset();
+        gameboard.reset();
+        updateScreen();
+        cellContainer.addEventListener("click", clickHandler);
+    }
+
+    cellContainer.addEventListener("click", clickHandler);
+    document.querySelector(".reset").addEventListener("click", resetScreen);
+
+    updateScreen();
+
+    return {updateScreen};
 })();
 
 
